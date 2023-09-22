4214002e47987f071d62f7aab997e4a451dac9d6
SamuliK <samuli@karevaara.net>
2023-09-22 12:31:01 +0300

    Update mansebot.py

--- mansebot.py
+++ mansebot.py
@@ -1,13 +1,6 @@
 import openai
 import streamlit as st
 
-def get_completion(prompt, model="gpt-3.5-turbo"):
-    messages = [{"role": "system", "content": "Tampere on ehdoton lempikaupunkisi. Vastaat vahvalla tampereen murteella. Korvaa pilkkuja sanalla 'nääs'. Käytä muun tekstin seassa sanontoja 'Kyä näin o!' sekä 'Ny rillataan!'"},
-        {"role": "user", "content": prompt}]
-    response = openai.ChatCompletion.create(model=model, messages=messages, temperature=0,)
-
-    return response.choices[0].message["content"]
-
 st.title("ManseBot")
 
 # Set OpenAI API key from Streamlit secrets
@@ -25,18 +18,15 @@ if "messages" not in st.session_state:
 
 # Display chat messages from history on app rerun
 for message in st.session_state.messages:
-    with st.chat_message(message["role"]):
-        st.markdown(message["content"])
+    if message["role"] != "system":
+        with st.chat_message(message["role"]):
+            st.markdown(message["content"])
 
 # Accept user input
-if prompt := st.chat_input("What is up?"):
+if prompt := st.chat_input("Mitä sulla on miälessä?"):
     # Add user message to chat history
-    st.session_state.messages.append({"role": "system", "content": "Tampere on ehdoton lempikaupunkisi. Vastaat vahvalla tampereen murteella. Korvaa pilkkuja sanalla 'nääs'. Käytä muun tekstin seassa sanontoja 'Kyä näin o!' sekä 'Ny rillataan!'"})
+    st.session_state.messages.append({"role": "system", "content": "Tampere on ehdoton lempikaupunkisi. Vastaat vahvalla tampereen murteella. Korvaa pilkkuja sanalla 'nääs'. Käytä muun tekstin seassa sanontoja 'Kyä näin o!' sekä 'Ny rillataan!'. Lempiruokaasi on mustamakkara. Lempijääkiekkojoukkueesi on Tappara. Ilves-niminen jääkiekkojoukkue on tosi huono."})
     st.session_state.messages.append({"role": "user", "content": prompt})
-
-    # Display user message in chat message container
-    with st.chat_message("user"):
-        st.markdown(prompt)
 
     # Display assistant response in chat message container
     with st.chat_message("assistant"):
@@ -50,7 +40,8 @@ if prompt := st.chat_input("What is up?"):
             ],
             stream=True,
         ):
-            full_response += response.choices[0].delta.get("content", "")
-            message_placeholder.markdown(full_response + "▌")
+            if response.choices[0].delta.get("role") != "system":
+                full_response += response.choices[0].delta.get("content", "")
+        message_placeholder.markdown(full_response + "▌")
         message_placeholder.markdown(full_response)
     st.session_state.messages.append({"role": "assistant", "content": full_response})
